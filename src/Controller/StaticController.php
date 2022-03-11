654d74dac7abb20b3c380a741ddfa0ea0a6cf97f
Norudah <pierucci.romain@gmail.com>
2022-03-11 16:03:22 +0100

    About us page

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -11,24 +11,24 @@ class StaticController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('static/index.html.twig', [
-            'controller_name' => 'StaticController',
-        ]);
+        return $this->render('static/index.html.twig');
     }
 
     #[Route('/what-we-do', name: 'what-we-do')]
     public function whatWeDo(): Response
     {
-        return $this->render('static/whatWeDo.html.twig', [
-            'controller_name' => 'StaticController',
-        ]);
+        return $this->render('static/whatWeDo.html.twig');
     }
 
     #[Route('/laboratory', name: 'laboratory')]
     public function laboratory(): Response
     {
-        return $this->render('static/laboratory.html.twig', [
-            'controller_name' => 'StaticController',
-        ]);
+        return $this->render('static/laboratory.html.twig');
+    }
+
+    #[Route('/about-us', name: 'about-us')]
+    public function AboutUs(): Response
+    {
+        return $this->render('static/aboutUs.html.twig');
     }
 }
